eb0f117fc6098b93e2ee9ce1f478fff74d58034b
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-12 22:50:59 +0430

    docs: adding react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

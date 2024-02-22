15a8719a0d33446db6a6e1ff5bb0a5df9d1765e5
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 15:10:49 +0430

    Style: Remove border from TrendingMovie component

--- src/components/TrendingMovie.jsx
+++ src/components/TrendingMovie.jsx
@@ -2,7 +2,7 @@ const TrendingMovie = ({ title, poster_path, vote_average }) => {
   const imageUrl = `https://image.tmdb.org/t/p/original/${poster_path}`;
   const imdbRating = vote_average.toFixed(1);
   return (
-    <div className="w-full mb-2 border border-slate-900 rounded-lg p-2 cursor-pointer">
+    <div className="w-full mb-2 rounded-lg p-2 cursor-pointer">
       <img
         src={imageUrl}
         alt="Movie Image"

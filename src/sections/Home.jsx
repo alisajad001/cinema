d8b4e092426beae0ae33f602b339be756b9bccfc
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 17:05:02 +0430

    Feat: Create MovieList component to fetching and displaying movies
    This commit introduces the MovieList component, which fetches movie data from the specified API endpoint and renders it on the page. The component provides a reusable structure for displaying different categories of movies with a consistent layout.

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,10 +1,11 @@
-import TrendingMovies from '../components/TrendingMovies';
+import MoviesList from '../components/MoviesList';
 
 const Home = () => {
   return (
     <main className="p-section bg-hero h-auto min-h-screen bg-cover text-white">
       <div className="max-w-[1280px] mx-auto">
-        <TrendingMovies />
+        <MoviesList title="Trending" apiEndpoint="trending/movie/week" />
+        <MoviesList title="Popular" apiEndpoint="movie/popular" />
       </div>
     </main>
   );

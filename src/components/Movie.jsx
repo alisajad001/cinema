84e726a7590886f52cd2961f5577a316a25addad
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 17:06:20 +0430

    Feat: Create Movie component
    This commit introduces the Movie component, responsible for rendering individual movie items. The component includes features such as displaying movie images, titles, IMDb ratings, and release dates. This new component enhances the overall movie representation in the application.

---
+++ src/components/Movie.jsx
@@ -0,0 +1,32 @@
+const Movie = ({ title, poster_path, vote_average, release_date }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+  const imdbRating = vote_average.toFixed(1);
+  const releaseDate = release_date.slice(0, 4);
+  const movieTitle = title.substring(0, 18);
+
+  return (
+    <div className="w-40 rounded-lg cursor-pointer transition flex-shrink-0">
+      <img
+        src={imageUrl}
+        alt="Movie Image"
+        className="rounded-lg hover:saturate-150 transition object-cover w-full"
+      />
+
+      <h3 className="text-sm font-semibold mt-2 text-white">
+        {movieTitle}...
+        <div className="flex justify-between mt-1">
+          <span className="block text-yellow-400 text-[10px]">
+            <span className="bg-yellow-400 text-dark px-[2px] rounded-sm font-bold">
+              IMDB
+            </span>{' '}
+            {imdbRating}
+          </span>
+
+          <span className="font-semibold text-[10px]">{releaseDate}</span>
+        </div>
+      </h3>
+    </div>
+  );
+};
+
+export default Movie;

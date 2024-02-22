9e58d74522b4d615ba5e102f4e074e50ac16b1f6
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 16:34:27 +0430

    Refactor: Adjust styles in TrendingMovie component

--- src/components/TrendingMovie.jsx
+++ src/components/TrendingMovie.jsx
@@ -1,22 +1,29 @@
-const TrendingMovie = ({ title, poster_path, vote_average }) => {
-  const imageUrl = `https://image.tmdb.org/t/p/original/${poster_path}`;
+const TrendingMovie = ({ title, poster_path, vote_average, release_date }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
   const imdbRating = vote_average.toFixed(1);
+  const releaseDate = release_date.slice(0, 4);
+
   return (
-    <div className="w-full mb-2 rounded-lg p-2 cursor-pointer">
+    <div className="w-40 mb-2 rounded-lg cursor-pointer transition flex-shrink-0">
       <img
         src={imageUrl}
         alt="Movie Image"
-        className="rounded-lg hover:saturate-150 transition object-cover"
+        className="rounded-lg hover:saturate-150 transition object-cover w-full h-60"
       />
 
-      <h3 className="flex justify-between items-center mt-2 text-sm font-semibold">
+      <h3 className="text-sm font-semibold mt-2 text-white">
         {title}
-        <span className="text-yellow-400 text-[10px]">
-          <span className="bg-yellow-400 text-dark px-[2px] rounded-sm font-bold">
-            IMDB
-          </span>{' '}
-          {imdbRating}
-        </span>
+
+        <div className="flex justify-between mt-1">
+          <span className="block text-yellow-400 text-[10px]">
+            <span className="bg-yellow-400 text-dark px-[2px] rounded-sm font-bold">
+              IMDB
+            </span>{' '}
+            {imdbRating}
+          </span>
+
+          <span className="font-semibold text-[10px]">{releaseDate}</span>
+        </div>
       </h3>
     </div>
   );

ae7e972a6fceaff478cd0239bbc77a1415486ebe
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 14:47:21 +0430

    feat: Implement movie fetching in TrendingMovies component using useFetch hook

--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,7 +1,12 @@
 import TrendingMovie from './TrendingMovie';
-import movieData from '../temporaryData';
+import useFetch from './hooks/useFetch';
 
 const TrendingMovies = () => {
+  const apiURL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${
+    import.meta.env.VITE_REACT_APP_TMDB_API_KEY
+  }`;
+  const { data, loading, error } = useFetch(apiURL);
+
   return (
     <section className="pt-5">
       <h2 className="text-xl sm:text-3xl text-white text-center font-bold">
@@ -9,9 +14,14 @@ const TrendingMovies = () => {
       </h2>
 
       <div className="mt-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 justify-items-center">
-        {movieData.map((movie) => (
-          <TrendingMovie key={movie.id} {...movie} />
-        ))}
+        {loading && <p>Loading...</p>}
+
+        {error && <p>Error: {error.message}</p>}
+
+        {data &&
+          data.results.map((movie) => (
+            <TrendingMovie key={movie.id} {...movie} />
+          ))}
       </div>
     </section>
   );

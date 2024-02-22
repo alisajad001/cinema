175d8d76a861294fa014aaec3a7571bb7fe320a4
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 14:48:57 +0430

    fix: Resolve issue with poster image in TrendingMovie component

--- src/components/TrendingMovie.jsx
+++ src/components/TrendingMovie.jsx
@@ -1,9 +1,9 @@
-const TrendingMovie = ({ title, image, rating }) => {
-  const reduceTitle = title.substring(0, 10);
+const TrendingMovie = ({ title, poster_path, rating }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/original/${poster_path}`;
   return (
     <div className="w-full mb-2 border border-slate-900 rounded-lg p-2 cursor-pointer">
       <img
-        src={image}
+        src={imageUrl}
         alt="Movie Image"
         className="rounded-lg hover:saturate-150 transition object-cover"
       />

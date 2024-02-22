6c2306086764818ba8295f62442160eb7187e094
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-22 14:45:18 +0430

    Add: Implement useFetch custom hook for fetching data

---
+++ src/components/hooks/useFetch.jsx
@@ -0,0 +1,35 @@
+// useFetch.jsx
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${
+              import.meta.env.VITE_REACT_APP_TMDB_API_KEY
+            }`,
+          },
+        });
+
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;

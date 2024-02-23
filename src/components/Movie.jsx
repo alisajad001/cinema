e2f9d73f0a1516df3ffdead2a303da92a48b0e85
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-23 09:11:24 +0430

    fix: Resolve title issues and add title tooltip to image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,12 +8,13 @@ const Movie = ({ title, poster_path, vote_average, release_date }) => {
     <div className="w-32 sm:w-44 rounded-lg cursor-pointer transition flex-shrink-0">
       <img
         src={movieImage}
-        alt="Movie Image"
+        alt={title}
+        title={title}
         className="rounded-lg hover:saturate-150 hover:-translate-y-2 transition object-cover w-full"
       />
 
       <h3 className="text-sm font-semibold mt-2 text-white">
-        {movieTitle}...
+        {title.length < 18 ? title : `${movieTitle}...`}
         <div className="flex justify-between mt-1">
           <span className="font-semibold text-[10px]">{movieReleaseDate}</span>
           <span className="block text-yellow-400 text-[10px]">

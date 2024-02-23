43c698a9040b0f862e68150aa51e4d1553889324
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-23 08:49:08 +0430

    refactor: move BrowserRouter from App to main.jsx for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './sections/Footer';
 import Home from './sections/Home';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 };
 

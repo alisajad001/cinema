70bdee6da99a98df8faf8a99c5ec5c8543a78e63
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-24 15:15:22 +0430

    Style: add padding to Home and Footer and update Footer text

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,11 +2,11 @@ import tmdbLogo from '../assets/tmdb-logo.svg';
 
 const Footer = () => {
   return (
-    <footer className="bg-hero text-white">
-      <div className="px-3 sm:container text-sm mx-auto flex flex-col sm:flex-row justify-between items-center py-4">
+    <footer className="bg-hero text-white text-center">
+      <div className="px-3 sm:container text-sm mx-auto flex flex-col sm:flex-row justify-between items-center py-7">
         <div className="flex flex-col sm:flex-row items-center gap-3">
           <img src={tmdbLogo} alt="TMDB Icon" width="60" />
-          <p>Powered by TMDB 2024</p>
+          <p>Powered by TMDB API and is not affiliated with TMDB</p>
         </div>
         <p className="mt-2 sm:mt-0">
           Made with <span className="text-secondary">❤</span> by Ali Sajad
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,7 +2,7 @@ import Welcome from '../components/Welcome';
 
 const Home = ({ children, query, setQuery }) => {
   return (
-    <main className="bg-hero h-auto min-h-screen bg-cover text-white">
+    <main className="bg-hero h-auto min-h-screen bg-cover text-white pb-20">
       <Welcome query={query} setQuery={setQuery} />
       <div className="px-3 sm:container mx-auto">{children}</div>
     </main>

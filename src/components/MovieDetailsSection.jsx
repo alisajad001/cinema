346874b8be3f98241e3161084d5c4cbdde0a620c
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-27 19:27:45 +0430

    feat: Add CastSection component to handle the display of movie cast

---
+++ src/components/MovieDetailsSection.jsx
@@ -0,0 +1,42 @@
+import { useParams } from 'react-router-dom';
+import useFetch from './hooks/useFetch';
+import MovieDetailsHeader from './MovieDetailsHeader';
+import MovieDetailsCast from './MovieDetailsCast';
+
+const MovieDetailsSection = () => {
+  const { id } = useParams();
+
+  const apiURL = `https://api.themoviedb.org/3/movie/${id}?`;
+
+  const apiCasts = `https://api.themoviedb.org/3/movie/${id}/credits?`;
+
+  const { data, loading, error } = useFetch(apiURL);
+  const {
+    data: casts,
+    loading: castLoading,
+    error: castError,
+  } = useFetch(apiCasts);
+
+  return (
+    <section className="h-auto text-white">
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <div className="w-full flex flex-col">
+          {/* Header */}
+          <MovieDetailsHeader data={data} />
+
+          {/* Cast */}
+          <MovieDetailsCast
+            casts={casts}
+            castLoading={castLoading}
+            castError={castError}
+            data={data}
+          />
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default MovieDetailsSection;

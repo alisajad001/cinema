4d140ccfbb98e99caac76bbd878963929f1e02fb
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-28 17:41:18 +0430

    fix: typo in import useFetch hook

--- src/components/MovieDetailsSection.jsx
+++ src/components/MovieDetailsSection.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import useFetch from './hooks/useFetch';
+import useFetch from '../hooks/useFetch';
 import MovieDetailsHeader from './MovieDetailsHeader';
 import MovieCastsList from './MovieCastsList';
 

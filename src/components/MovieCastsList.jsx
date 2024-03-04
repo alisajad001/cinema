6e71a1daf4206464a4a387b0029b3dca96bf935d
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-04 22:22:13 +0430

    fix: length error fixed

--- src/components/MovieCastsList.jsx
+++ src/components/MovieCastsList.jsx
@@ -13,18 +13,18 @@ const MovieCastsList = ({ casts, castLoading, castError, data }) => {
 
   return (
     <section className="flex flex-col-reverse w-full max-w-6xl mx-auto sm:flex-row-reverse mb-12">
-      {casts.length > 0 ? (
-        <>
-          <div className="p-4 sm:container mx-auto">
+      <div className="p-4 sm:container mx-auto">
+        {casts ? (
+          <>
             <h3 className="text-lg font-semibold mb-5">Cast</h3>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 justify-items-center gap-6">
               {casts.cast.map((cast) => (
                 <MovieCast key={cast.id} cast={cast} />
               ))}
             </div>
-          </div>
-        </>
-      ) : null}
+          </>
+        ) : null}
+      </div>
 
       {data ? <MovieDetails data={data} /> : <p>No movie details available.</p>}
     </section>
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,15 +2,30 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const useFetch = (endpoint) => {
+  const cancelTokenSource = axios.CancelToken.source();
+
   const fetchData = async () => {
-    const response = await axios.get(
-      `${endpoint}api_key=${import.meta.env.VITE_REACT_APP_TMDB_API_KEY}`,
-    );
-    return response.data;
+    try {
+      const response = await axios.get(
+        `${endpoint}api_key=${import.meta.env.VITE_REACT_APP_TMDB_API_KEY}`,
+        { cancelToken: cancelTokenSource.token },
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log('Request was cancelled.');
+      } else {
+        console.error('Error occurred during data fetching.', error);
+        throw error;
+      }
+    }
   };
 
   const { data, error, isLoading } = useQuery(endpoint, fetchData, {
     refetchOnWindowFocus: false,
+    // The onCancel function is called when the query is cancelled
+    onCancel: () =>
+      cancelTokenSource.cancel('Query was cancelled by React Query.'),
   });
 
   return { data, isLoading, error };

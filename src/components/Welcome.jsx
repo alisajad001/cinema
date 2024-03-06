e6df7184b3560b08add910ff1f26ec571434e7aa
alisajad001 <forwebonly0@gmail.com>
2024-03-06 22:56:04 +0430

    refactor: Remove welcome text and update style for better UI

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,20 +1,17 @@
 const Welcome = ({ query, setQuery }) => {
   return (
-    <section className="h-96 mb-10 flex items-center bg-gradientPrimary">
-      <div className="px-3 sm:container mx-auto w-full text-center sm:text-left">
+    <section className="h-72 mb-10 flex items-center bg-gradientPrimary">
+      <div className="px-3 sm:container mx-auto w-full text-center">
         <h1 className="text-3xl sm:text-5xl font-bold">
-          Welcome to <span className="text-primary">Cinema.</span>
+          CINEMA <span className="text-primary">MDB</span>
         </h1>
-        <p className="text-white/80 text-sm sm:text-xl mt-3">
-          Find the world's largest collection of movies.
-        </p>
 
         <input
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           type="text"
           placeholder="Search for a movie..."
-          className="w-full bg-white/10 border border-white/20 outline-none rounded-full p-4 mt-10"
+          className="w-full sm:w-[90%] md:w-[32rem] bg-white/10 border border-white/20 outline-none rounded-full p-4 mt-5"
         />
       </div>
     </section>

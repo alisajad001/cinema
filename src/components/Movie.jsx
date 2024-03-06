41a8cb72247b862c28662efe160e300f9437663d
alisajad001 <forwebonly0@gmail.com>
2024-03-06 22:49:08 +0430

    style: Increase image width to 60 on bigger devices

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -14,7 +14,7 @@ const Movie = ({ title, poster_path, vote_average, release_date, id }) => {
 
   return (
     <div
-      className="w-32 sm:w-44 rounded-lg cursor-pointer transition flex-shrink-0"
+      className="w-32 sm:w-60 rounded-lg cursor-pointer transition flex-shrink-0"
       onClick={handleClick}
     >
       <img
@@ -30,16 +30,16 @@ const Movie = ({ title, poster_path, vote_average, release_date, id }) => {
 
       <h3 className="text-sm font-semibold mt-2 text-white">
         {title.length < 18 ? title : `${movieTitle}...`}
-        <div className="flex justify-between mt-1">
-          <span className="font-semibold text-[10px]">{movieReleaseDate}</span>
-          <span className="block text-yellow-400 text-[10px]">
-            <span className="bg-yellow-400 text-dark px-[2px] rounded-sm font-bold">
-              IMDB
-            </span>{' '}
-            {movieRating}
-          </span>
-        </div>
       </h3>
+      <div className="flex justify-between mt-1">
+        <span className="font-semibold text-[10px]">{movieReleaseDate}</span>
+        <span className="block text-yellow-400 text-[10px]">
+          <span className="bg-yellow-400 text-dark px-[2px] rounded-sm font-bold">
+            IMDB
+          </span>{' '}
+          {movieRating}
+        </span>
+      </div>
     </div>
   );
 };

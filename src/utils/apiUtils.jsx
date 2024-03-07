b3e3d437ecb91fae1b5c35b9978bef5d5c604e88
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-07 23:29:37 +0430

    Add: New helper function, getMovieCasts for better code readability

--- src/utils/apiUtils.jsx
+++ src/utils/apiUtils.jsx
@@ -4,4 +4,8 @@ const getMovieEndpoint = (query) => `search/movie?query=${query}&`;
 // Get Movie by ID
 const getMovieById = (id) => `https://api.themoviedb.org/3/movie/${id}?`;
 
-export { getMovieEndpoint, getMovieById };
+// Get Movie Casts
+const getMovieCasts = (id) =>
+  `https://api.themoviedb.org/3/movie/${id}/credits?`;
+
+export { getMovieEndpoint, getMovieById, getMovieCasts };

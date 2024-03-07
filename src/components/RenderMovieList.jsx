1a3448ba1de328a0f63910d9f29c88fad954797a
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-07 23:24:40 +0430

    refactor: Use the newly changed helper function to handle movie search

--- src/components/RenderMovieList.jsx
+++ src/components/RenderMovieList.jsx
@@ -1,8 +1,8 @@
-import { getApiURL } from '../utils/apiUtils';
+import { getMovieEndpoint } from '../utils/apiUtils';
 import MoviesList from './MoviesList';
 
 const RenderMoviesList = ({ query }) => {
-  const apiURL = getApiURL(query);
+  const apiURL = getMovieEndpoint(query);
 
   return query && query.length > 0 ? (
     <MoviesList apiEndpoint={apiURL} title="Search result" />

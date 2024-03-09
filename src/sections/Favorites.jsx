4f3ebb5b11b5e164611560cea3ad9aa79dfd86c7
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-09 15:44:23 +0430

    add: Page heading title

--- src/sections/Favorites.jsx
+++ src/sections/Favorites.jsx
@@ -1,5 +1,15 @@
+import Movie from '../components/Movie';
+
 const Favorites = () => {
-  return <section className="h-screen">Favorites</section>;
+  return (
+    <section className="h-screen">
+      <div className="container mx-auto py-7">
+        <h2 className="text-lg m:text-2xl font-semibold text-white">
+          Favorites
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default Favorites;

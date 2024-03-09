c5a9ace0edae11576a4cdf1daea56d4d8c99c1dc
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-09 14:01:27 +0430

    feat: Handle loading

--- src/components/MovieCastDetails.jsx
+++ src/components/MovieCastDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import MovieCastDetailsHeader from './MovieCastDetailsHeader';
+import Loader from './Loader';
 
 const MovieCastDetails = () => {
   const { castId } = useParams();
@@ -9,13 +10,15 @@ const MovieCastDetails = () => {
 
   const knownForAPI = `https://api.themoviedb.org/3/person/${castId}/movie_credits?`;
 
-  const { data } = useFetch(personAPI);
+  const { data, isLoading } = useFetch(personAPI);
   const { data: knownFor } = useFetch(knownForAPI);
 
   return (
     <div>
-      {/* {isLoading && 'Loading...'} */}
-      {/* {isError && error.message} */}
+      {/* Loading */}
+      {isLoading && (
+        <Loader className="h-screen flex items-center justify-center" />
+      )}
 
       {data && <MovieCastDetailsHeader personData={data} knownFor={knownFor} />}
     </div>

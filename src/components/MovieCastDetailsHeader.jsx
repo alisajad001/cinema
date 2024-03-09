4927052bf700984d4c1e22b5307e46f109746a2e
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-09 14:03:53 +0430

    feat: Handle knownFor data and use it

--- src/components/MovieCastDetailsHeader.jsx
+++ src/components/MovieCastDetailsHeader.jsx
@@ -1,14 +1,16 @@
+import Movie from './Movie';
+
 const MovieCastDetailsHeader = ({ personData, knownFor }) => {
   const { name, birthday, profile_path, place_of_birth, biography } =
     personData;
 
   return (
-    <div className="flex flex-col sm:flex-row py-12 px-3 sm:px-10">
-      <div className="w-full sm:w-1/2 flex flex-col items-center sm:items-start justify-center">
+    <div className="flex flex-col md:flex-row py-12 px-3 sm:px-10 gap-5 items-center md:items-start">
+      <div className="w-full  sm:w-1/2 flex flex-col items-center sm:items-start sm:justify-start justify-center">
         <img
           src={`https://image.tmdb.org/t/p/w500/${profile_path}`}
           alt={name || 'Actor Profile'}
-          className="w-72 sm:w-80 h-[30rem] object-cover rounded-md"
+          className="w-72 sm:w-80 object-cover rounded-md"
         />
 
         <div className="flex flex-col bg-hero rounded-md mt-5 w-72 sm:w-80 p-3">
@@ -26,6 +28,25 @@ const MovieCastDetailsHeader = ({ personData, knownFor }) => {
           <p className="text-primary">
             Biography: <span className="text-white/60">{biography}</span>
           </p>
+        </div>
+      </div>
+
+      <div className="mt-20 sm:mt-0 w-full h-auto">
+        <h2 className="text-white text-2xl mb-5 font-semibold">Known for</h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 justify-items-center gap-4">
+          {knownFor &&
+            knownFor.cast.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.title}
+                  poster_path={movie.poster_path}
+                  release_date={movie.release_date}
+                  vote_average={movie.vote_average}
+                />
+              );
+            })}
         </div>
       </div>
     </div>

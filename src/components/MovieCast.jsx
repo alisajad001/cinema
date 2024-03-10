2573ffe9ba5bc842b1415a999760fd34c459f968
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-10 22:11:04 +0430

    style: Refactor zoom hover effect

--- src/components/MovieCast.jsx
+++ src/components/MovieCast.jsx
@@ -11,18 +11,21 @@ const MovieCast = ({ cast }) => {
   return (
     <div
       key={cast.id}
-      className="flex flex-col items-center text-center cursor-pointer relative overflow-hidden"
+      className="flex flex-col items-center text-center cursor-pointer"
       onClick={handleClick}
     >
-      <img
-        src={
-          cast.profile_path
-            ? `https://image.tmdb.org/t/p/w500/${cast.profile_path}`
-            : 'https://placehold.co/300x450/001/FFF?text=Image not found'
-        }
-        alt={cast.name || 'Actor Profile'}
-        className="rounded-sm object-cover hover:scale-105 transition-all"
-      />
+      <div className="relative overflow-hidden rounded-sm">
+        <img
+          src={
+            cast.profile_path
+              ? `https://image.tmdb.org/t/p/w500/${cast.profile_path}`
+              : 'https://placehold.co/300x450/001/FFF?text=Image not found'
+          }
+          alt={cast.name || 'Actor Profile'}
+          className="rounded-sm object-cover hover:scale-105 transition-all"
+        />
+      </div>
+
       <h3 className="mt-2 font-semibold">{cast.name}</h3>
       <p className="text-primary text-sm">
         {cast.character ? (

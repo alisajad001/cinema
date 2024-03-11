b1287830d4d657c5f109cc0c6b9924abda6ec4f9
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-11 22:23:29 +0430

    Feat: Recieve and pass addToFavorites prop to Movie.jsx

--- src/sections/Favorites.jsx
+++ src/sections/Favorites.jsx
@@ -1,15 +1,25 @@
 import Movie from '../components/MovieList/Movie';
 
-const Favorites = ({ favoriteMovies }) => {
+const Favorites = ({ favoriteMovies, addToFavorites }) => {
   return (
     <section className="p-5">
       <h2 className="text-white text-2xl font-bold mb-7">Favorites</h2>
       <div className="container mx-auto flex gap-6 flex-wrap justify-center">
-        {favoriteMovies.map((movie) => {
-          return (
-            <Movie key={movie.id} data={movie} id={movie.id} className="w-52" />
-          );
-        })}
+        {favoriteMovies.length > 0 ? (
+          favoriteMovies.map((movie) => {
+            return (
+              <Movie
+                key={movie.id}
+                data={movie}
+                id={movie.id}
+                addToFavorites={addToFavorites}
+                className="w-52"
+              />
+            );
+          })
+        ) : (
+          <p className="h-screen text-white">Nothing in your favorite list.</p>
+        )}
       </div>
     </section>
   );

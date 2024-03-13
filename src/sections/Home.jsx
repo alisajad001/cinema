f6838b51e729be1fbc08a072b2ab26fbf26fcc46
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-13 19:38:19 +0430

    Style: Change padding

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,7 +2,7 @@ import Welcome from '../components/Welcome';
 
 const Home = ({ children, query, setQuery }) => {
   return (
-    <main className="text-white pb-20">
+    <main className="text-white py-20">
       <Welcome query={query} setQuery={setQuery} />
       <div className="px-3 sm:container mx-auto">{children}</div>
     </main>

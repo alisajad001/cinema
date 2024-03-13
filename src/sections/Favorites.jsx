3ce7a09919db9bcaa2288295660bf0efa95f2317
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-13 19:57:22 +0430

    Style: Change padding

--- src/sections/Favorites.jsx
+++ src/sections/Favorites.jsx
@@ -3,7 +3,7 @@ import Movie from '../components/MovieList/Movie';
 
 const Favorites = ({ favoriteMovies, addToFavorites }) => {
   return (
-    <section className="p-5">
+    <section className="py-20 px-5">
       <h2 className="text-white text-2xl font-bold mb-7">Favorites</h2>
       <ToastContainer position="bottom-right" autoClose={3000} />
       <div className="container mx-auto gap-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center">

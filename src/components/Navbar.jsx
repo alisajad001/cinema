15fd0865448772da6b3ec513c5d2549fea2ee4ba
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-13 19:37:35 +0430

    Style: Make Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <header className="p-section bg-dark text-white flex justify-between items-center">
+    <header className="p-section bg-dark text-white flex justify-between items-center fixed w-full z-50">
       <Link to="/" className="font-extrabold uppercase">
         Cinema <span className="text-primary">MDB</span>
       </Link>
